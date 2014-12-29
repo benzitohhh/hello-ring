97ce66a9160e5a984a3589ade941ad70b732662f
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2014-12-29 14:26:18 -0300

    rung stuff working

--- src/hello_ring/core.clj
+++ src/hello_ring/core.clj
@@ -1,7 +1,34 @@
 (ns hello-ring.core
-  (:gen-class))
+  (:gen-class)
+  (:require [ring.adapter.jetty]
+            [ring.middleware.stacktrace]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<h1>Hello World!</h1>" (/ 5 0))})
+
+(defn wrap-spy [handler]
+  (fn [request]
+    (println "-------------------------------")
+    (println "Incoming Request:")
+    (clojure.pprint/pprint request)
+    (let [response (handler request)]
+      (println "Outgoing Response Map:")
+      (clojure.pprint/pprint response)
+      (println "-------------------------------")
+      response)))
+
+(def app
+  (-> #'handler ;; passing functions as a Var, allows us to change the function dynamically 
+      (ring.middleware.stacktrace/wrap-stacktrace) ;; send back exception in HTML
+      ;(wrap-spy)
+      ))
+
+(defonce server (ring.adapter.jetty/run-jetty #'app {:port 8080 :join? false}))
+
